0bcaed03884c1e1ed20472361905e1afbd4c5133
Emilia Piotrowska <135655423+piatekwmarcu@users.noreply.github.com>
2026-04-11 12:17:55 +0000

    Add new folder 02_task_allocation_in_multi-processor_system with task_allocation.py

---
+++ 02_task_allocation_in_multi-processor_system/task_allocation.py
@@ -0,0 +1,9 @@
+# Task Allocation in Multi-Processor System
+# Solution implementation
+
+def main():
+    # TODO: Implement task allocation algorithm
+    pass
+
+if __name__ == "__main__":
+    main()
